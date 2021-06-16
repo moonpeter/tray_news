2c6ccf695a7a7d760de2d3619e559cb46df7213b
moonpeter <moonpeter@kakao.com>
2021-06-16 14:26:31 +0900

    [21.06.16] in trayd
    Cnet - 5분마다 크롤링 되도록 1차 완료

--- crawler/parser_cnet.py
+++ crawler/parser_cnet.py
@@ -7,22 +7,26 @@ from bs4 import BeautifulSoup
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 import django
-
 django.setup()
+
+import urllib3
+urllib3.disable_warnings()
 
 from crawler.models import CrawlingData
 
 
 def get_list():
     print(" ========== start crawling ========== ")
-    webpage = requests.get("https://www.cnet.com/news/")
+    webpage = requests.get("https://www.cnet.com/news/", verify=False)
     soup = BeautifulSoup(webpage.content, "html.parser")
 
     the_latest = soup.find("div", {"class": "col-8 fdListing"})
     cnet_title = the_latest.findAll(attrs={'class': 'assetHed'})
 
+    if len(CrawlingData.objects.all()) > 0:
+        get_title_in_db()
+
     for i in range(10, 0, -2):
-        # if i % 2 == 0:
         title = cnet_title[i].text.strip()
 
         content_page = "https://www.cnet.com" + cnet_title[i]['href']
@@ -31,6 +35,7 @@ def get_list():
         sub_title = content_in_p[0].text.strip()
 
         main_content = ''
+
         for j in range(1, len(content_in_p)):
             if content_in_p[j].parent.parent.name == "figcaption":
                 continue
@@ -40,10 +45,8 @@ def get_list():
             except KeyError:
                 pass
             main_content += content_in_p[j].text
-        if len(CrawlingData.objects.all()) > 0:
-            get_title_in_db()
 
-        if title not in title_list_in_db[0:4]:
+        if title not in title_list_in_db:
             crawling_data = CrawlingData(
                 news_site='Cnet',
                 eng_title=title,
@@ -53,7 +56,7 @@ def get_list():
             crawling_data.save()
     print("get_list() === time : ", datetime.datetime.now())
     print(" ========== finished crawling ========== ")
-    return
+    return title_list_in_db.clear()
 
 
 title_list_in_db = []
@@ -61,9 +64,10 @@ title_list_in_db = []
 
 # DB에서 최신 5개의 제목 정보를 가져오는 메서드
 def get_title_in_db():
-    for i in range(0, len(CrawlingData.objects.all())):
+    for i in range(0, 5):
         title_list_in_db.append(CrawlingData.objects.all().order_by('-id').values('eng_title')[i]['eng_title'])
-    print(len(CrawlingData.objects.all()), "1111111111")
+    print("CrawlingData의 갯수 ? ", len(CrawlingData.objects.all()))
+    print("title_list_in_db : ", title_list_in_db)
     return title_list_in_db
 
 
--- crawler/scheduler_for_crawling.py
+++ crawler/scheduler_for_crawling.py
@@ -4,10 +4,11 @@ import schedule
 
 from crawler.parser_cnet import get_list
 
+
 # 5초마다 실행
-schedule.every(5).seconds.do(get_list)
+# schedule.every(5).seconds.do(get_list)
 # 5분마다 실행
-# schedule.every(5).minutes.do(get_list)
+schedule.every(5).minutes.do(get_list)
 
 while True:
     schedule.run_pending()
