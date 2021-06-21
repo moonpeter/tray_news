cec4d7be6bd3d70d92d5a641f7526768e4939775
moonpeter <moonpeter@kakao.com>
2021-06-21 16:45:16 +0900

    [21.06.21] in trayd
    DB model 내용 수정

--- crawler/models.py
+++ crawler/models.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 
 
 class CrawlingData(models.Model):
-    news_site = models.CharField(max_length=30, blank=True)
-    eng_title = models.CharField(max_length=200, blank=True)
-    eng_sub_title = models.CharField(max_length=200, blank=True)
-    eng_content = models.TextField(blank=True)
-    ko_title = models.CharField(max_length=200, blank=True)
-    ko_sub_title = models.CharField(max_length=200, blank=True)
-    ko_content = models.TextField(blank=True)
+    news_site = models.CharField(max_length=50, default='')
+    post_date = models.DateTimeField(default=datetime.now)
+    eng_title = models.CharField(max_length=200, default='')
+    eng_sub_title = models.CharField(max_length=200, default='')
+    eng_content = models.TextField(default='')
+    ko_title = models.CharField(max_length=200, default='')
+    ko_sub_title = models.CharField(max_length=200, default='')
+    ko_content = models.TextField(default='')
